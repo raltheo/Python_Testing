cb1ec64ba26d81101ce10ada991b35e6787e3527
Théo <cobaltium360@gmail.com>
2024-03-19 22:29:56 +0100

    ajout de la feature voir readme

--- server.py
+++ server.py
@@ -20,6 +20,9 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+def load_other(our_club):
+    return  [club for club in clubs if club != our_club]
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -28,18 +31,20 @@ def index():
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']]
     if not club: return abort(404)
-    return render_template('welcome.html',club=club[0],competitions=competitions)
+    return render_template('welcome.html',club=club[0],competitions=competitions, other_clubs=load_other(club[0]))
 
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    foundClub = [c for c in clubs if c['name'] == club]
+    if not foundClub : return abort(404)
+    foundCompetition = [c for c in competitions if c['name'] == competition]
+    if not foundCompetition : return abort(404)
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html',club=foundClub[0],competition=foundCompetition[0])
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=foundClub[0], competitions=competitions, other_clubs=load_other(foundClub[0]))
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -52,22 +57,22 @@ def purchasePlaces():
 
     if datetime.strptime(competition[0]["date"], "%Y-%m-%d %H:%M:%S") < datetime.now():
         flash('Can\'t buy, the competion has ended!')#added for issue 5
-        return render_template('welcome.html', club=club[0], competitions=competitions)
+        return render_template('welcome.html', club=club[0], competitions=competitions, other_clubs=load_other(club[0]))
     
     placesRequired = int(request.form['places'])
     if placesRequired > int(competition[0]["numberOfPlaces"]):
         flash('Can\'t buy more than available places!')
-        return render_template('welcome.html', club=club[0], competitions=competitions)
+        return render_template('welcome.html', club=club[0], competitions=competitions, other_clubs=load_other(club[0]))
     
     pointsclub = int(club[0]["points"]) #added for issue 2
     if pointsclub < placesRequired or placesRequired > 12: #added for issue 2  and issue 4
         flash('Can\'t buy more than your points or more than 12 places!')#added for issue 2  and issue 4
-        return render_template('welcome.html', club=club[0], competitions=competitions)#added for issue 2
+        return render_template('welcome.html', club=club[0], competitions=competitions, other_clubs=load_other(club[0]))#added for issue 2
     
     competition[0]['numberOfPlaces'] = int(competition[0]['numberOfPlaces'])-placesRequired
     club[0]["points"] = int(club[0]["points"])-placesRequired#issue 6
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club[0], competitions=competitions)
+    return render_template('welcome.html', club=club[0], competitions=competitions, other_clubs=load_other(club[0]))
 
 
 # TODO: Add route for points display
--- tests/feature_display_clubs/readme.md
+++ tests/feature_display_clubs/readme.md
@@ -1,6 +1,18 @@
 ## Résumé de la feature
 
-Ce MAJ permets d'afficher le noms des autres clubs et leur nombres de points
+Cette  MAJ permets d'afficher le noms des autres clubs et leur nombres de points
 
 before creation of feature :
 ![alt text](image.png)
+
+## Description de la MAJ
+
+J'ai modifié la template welcome pour recevoir de nouveau parametre `other_clubs`, j'ai fais une fonction pour retourné les clubs sans notre club actuel et je display leur name et leur nombre de points.
+
+```py
+def load_other(our_club):
+    return  [club for club in clubs if club != our_club]
+```
+
+after creation of feature:
+![alt text](image-1.png)
